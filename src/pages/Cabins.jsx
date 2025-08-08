8aaed7b4defe3e86b67b1fbfcd7c76e7ccc9fbff
nkieu-config <natthachak.config@gmail.com>
2025-08-09 01:59:21 +0700

    feat: implement filtering and sorting for cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,3 +1,4 @@
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
 import Heading from "../ui/Heading";
@@ -8,7 +9,7 @@ function Cabins() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row>
