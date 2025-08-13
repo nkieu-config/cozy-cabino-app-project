25781608dcc46fb89311de8474942c426e1202b8
nkieu-config <natthachak.config@gmail.com>
2025-08-14 05:21:36 +0700

    fix: update Supabase configuration to use environment variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://eaojmlsmihpqgucajvcy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhb2ptbHNtaWhwcWd1Y2FqdmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNTkxODAsImV4cCI6MjA2OTczNTE4MH0.ORwbsXNTivtBkO-KwR9ag60iBUInwX3TIUXKlRKKT8I";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
